b6029c52f89b157a125d659b96eb454cd3ac9a0a
oldcoyote03 <nickolas.vela@gmail.com>
2022-06-19 17:22:31 -0700

    app settings to separate file

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 
-import uuid
 from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 from sqlalchemy.sql import func
 from datetime import date
@@ -14,19 +14,19 @@ from random import Random
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'cockroachdb://root@haproxy:26257/tagger_db?sslmode=disable'
+app.config.from_object("settings")
 db = SQLAlchemy(app)
 api = Api(app)
 
 class TestTable(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True)
-    created_at = db.Column(db.Date, default=func.current_date(type=db.Date,inherit_cache=False))
-    #created_at = db.Column(db.Date, default=date.today().isoformat())
+    created_at = db.Column(
+        db.Date, 
+        default=func.current_date(type=db.Date,inherit_cache=False)
+    )
 
     def __repr__(self):
         return f'<Test Table {self.id}>'
-
 
 
 class HelloWorld(Resource):
@@ -46,3 +46,4 @@ api.add_resource(HelloWorld, '/hello')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
---
+++ settings.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'cockroachdb://root@haproxy:26257/tagger_db?sslmode=disable'

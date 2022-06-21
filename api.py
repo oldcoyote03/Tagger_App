d5f2f3f4a0a81d6dbaf1eda8b2d7ffdbdbd2802d
oldcoyote03 <nickolas.vela@gmail.com>
2022-06-20 20:26:30 -0700

    implicit marshmallow sqlalchemy integration

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_marshmallow import Marshmallow
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+#from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
@@ -30,7 +30,7 @@ class TestTable(db.Model):
         return f'<Test Table {self.id}>'
 
 
-class TestTableSchema(SQLAlchemyAutoSchema):
+class TestTableSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = TestTable
 

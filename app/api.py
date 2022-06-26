517b5076de01a32a9464a528a06dc6052c4481e2
oldcoyote03 <nickolas.vela@gmail.com>
2022-06-25 22:33:03 -0700

    get single, multiple and post single no payload

--- app/api.py
+++ app/api.py
@@ -1,16 +1,26 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from app.schema import db, ma, Bookmarks, BookmarksSchema
 import uuid
 
-bookmarks_schema = BookmarksSchema()
+parser = reqparse.RequestParser()
+
+bookmark_schema = BookmarksSchema()
+bookmarks_schema = BookmarksSchema(many=True)
 
 class BookmarksResource(Resource):
     def get(self):
-        my_uuid = uuid.uuid4()
-        bookmark = Bookmarks(id=my_uuid)
+        all_bookmarks = Bookmarks.all()
+        return bookmarks_schema.dump(all_bookmarks)
+
+    def post(self):
+        bookmark = Bookmarks(id=uuid.uuid4())
         db.session.add(bookmark)
         db.session.commit()
-        returned_bookmark = Bookmarks.query.get_or_404(my_uuid)
-        dump_bookmark = bookmarks_schema.dump(returned_bookmark)
         return dump_bookmark
+
+class BookmarkResource(Resource):
+    def get(self):
+        returned_bookmark = Bookmarks.query.get_or_404(my_uuid)
+        return bookmark_schema.dump(returned_bookmark)
+

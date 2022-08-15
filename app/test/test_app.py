a8df097f3153bc95a136de2e5284d28137ce6d03
oldcoyote03 <nickolas.vela@gmail.com>
2022-08-14 19:31:51 -0700

    initial test for post

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -18,6 +18,14 @@ def test_endpoint(client):
     assert 'msg' in data_obj
     assert data_obj['msg'] == "This is the test endpoint"
 
+def test_post_bookmarks(client):
+    payload = { "url": "https://www.imdb.com" }
+    response = client.post('/bookmarks', json=payload)
+    assert response.status_code == 200
+
+    data = response.get_data()
+    assert data == "post"
+
 def test_get_bookmarks(client):
     response = client.get('/bookmarks')
     assert response.status_code == 200

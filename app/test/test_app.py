bb7b5022f8baed58eb262d59681c6dceb3a72e3a
oldcoyote03 <nickolas.vela@gmail.com>
2022-08-14 19:18:35 -0700

    test get bookmarks

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -1,11 +1,13 @@
 
 import json
 
+"""
 def test_always_passes():
     assert True
 
 def test_always_fails():
     assert False
+"""
 
 def test_endpoint(client):
     response = client.get('/test')
@@ -15,4 +17,11 @@ def test_endpoint(client):
     data_obj = json.loads(data)
     assert 'msg' in data_obj
     assert data_obj['msg'] == "This is the test endpoint"
-    
+
+def test_get_bookmarks(client):
+    response = client.get('/bookmarks')
+    assert response.status_code == 200
+
+    data = response.get_data()
+    data_obj = json.loads(data)
+    assert len(data_obj) == 0

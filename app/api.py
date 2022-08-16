e3e43d26381cf250f375b66817a5afa39516ef9e
oldcoyote03 <nickolas.vela@gmail.com>
2022-08-16 12:14:50 -0700

    delete msg

--- app/api.py
+++ app/api.py
@@ -43,7 +43,7 @@ class BookmarkResource(Resource):
         bookmark = Bookmarks.query.get_or_404(bookmark_id)
         db.session.delete(bookmark)
         db.session.commit()
-        return 'delete /{}'.format(bookmark_id), 204
+        return { 'msg': 'delete /{}'.format(bookmark_id), 204 }
 
 
 from flask import jsonify

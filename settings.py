97e86b9e9f33e35d0997501c116990b6a10479dd
oldcoyote03 <nickolas.vela@gmail.com>
2022-09-03 22:20:07 -0700

    use k8s env var for db host

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 s = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_DATABASE_URI = s.replace(
     s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
-    os.environ.get('DATABASE_HOST')
+    os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST')
 )
 #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')

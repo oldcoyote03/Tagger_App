ecbb2156a09d57b642173d75bcf7a723d202b5e6
oldcoyote03 <nickolas.vela@gmail.com>
2022-09-03 22:09:19 -0700

    decouple db host from db conn

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+s = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config["SQLALCHEMY_DATABASE_URI"] = s.replace(
+    s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
+    os.environ.get('DATABASE_HOST')
+)
+#SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')

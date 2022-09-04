87cd602fba87b1a96ea85d2b2408e82c7734e000
oldcoyote03 <nickolas.vela@gmail.com>
2022-09-03 22:23:00 -0700

    sqlalchemy database uri

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-s = os.environ.get('SQLALCHEMY_DATABASE_URI')
-SQLALCHEMY_DATABASE_URI = s.replace(
-    s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
-    os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST')
-)
-#SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+if os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST') not None:
+    s = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = s.replace(
+        s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
+        os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST')
+    )
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')

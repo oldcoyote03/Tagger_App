11f9d1902037a5d644c45bc0ca15d5c6c32d0b42
oldcoyote03 <nickolas.vela@gmail.com>
2022-09-03 21:47:10 -0700

    comment out passable db conn

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,14 @@ import sys
 def create_app():
     app = Flask(__name__)
     app.config.from_object("settings")
+    """
     if len(sys.argv) > 1:
         s = app.config["SQLALCHEMY_DATABASE_URI"]
         app.config["SQLALCHEMY_DATABASE_URI"] = s.replace(
             s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
             sys.argv[1]
         )
+    """
     db.init_app(app)
     api = Api(app)
     ma.init_app(app)

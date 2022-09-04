577019f0af6134198a35516e74aa41e7ec1c130f
oldcoyote03 <nickolas.vela@gmail.com>
2022-09-03 22:27:40 -0700

    if syntax

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-if os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST') not None:
+if os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST') is not None:
     s = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = s.replace(
         s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 

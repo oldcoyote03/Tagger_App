a9d9ee17e7e8d3f3e429b65f7ae60f61d9ed9631
oldcoyote03 <nickolas.vela@gmail.com>
2022-09-09 14:48:05 -0700

    assume infra puts connection string in env var

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-s = os.environ.get('SQLALCHEMY_DATABASE_URI')
-if os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST') is not None:
-    s = s.replace(
-        s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
-        os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST')
-    )
-SQLALCHEMY_DATABASE_URI = s
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+# not ideal for app to be aware of infra layer details
+# s = os.environ.get('SQLALCHEMY_DATABASE_URI')
+# if os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST') is not None:
+#     s = s.replace(
+#         s[ s.find('@')+1 : s.find(':', s.find('@')+1) ], 
+#         os.environ.get('TDB_COCKROACHDB_PUBLIC_SERVICE_HOST')
+#     )
+# SQLALCHEMY_DATABASE_URI = s

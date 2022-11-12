409605495cc7e4dc1f8637343bf27b67fe0d9e2e
oldcoyote03 <nickolas.vela@gmail.com>
2022-11-11 16:31:39 -0800

    app context

--- app/test/test_mock_app.py
+++ app/test/test_mock_app.py
@@ -5,10 +5,12 @@ import json
 import uuid
 
 def test_endpoint(flask_app_mock):
-    response = flask_app_mock.get(url_for('testresource'))
-    assert response.status_code == 200
+    with flask_app_mock..app_context():
+        #response = flask_app_mock.get(url_for('testresource'))
+        response = get(url_for('testresource'))
+        assert response.status_code == 200
 
-    data = response.get_data()
-    data_obj = json.loads(data)
-    assert 'msg' in data_obj
-    assert data_obj['msg'] == "This is the test endpoint"
+        data = response.get_data()
+        data_obj = json.loads(data)
+        assert 'msg' in data_obj
+        assert data_obj['msg'] == "This is the test endpoint"

b505a6b7d883a379c2ba0aa1229ec5bffd7d0cb0
oldcoyote03 <nickolas.vela@gmail.com>
2024-07-18 15:28:13 -0500

    platform agnostic UUID type

--- app/schema.py
+++ app/schema.py
@@ -2,9 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from sqlalchemy_utils.types.uuid import UUIDType
 from sqlalchemy.types import Uuid
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 
 db = SQLAlchemy()
@@ -13,26 +11,7 @@ ma = Marshmallow()
 class Bookmarks(db.Model):
     """ Bookmarks schema """
 
-    # cbd resolves to BINARY; sqlite resolves to ?
-    # no type conversion at API layer
-    id = db.Column(UUIDType(), primary_key=True)  # cdb resolves to BINARY
-    
-    # all resolve to CHAR
-    # no type conversions at API layer
-    # id = db.Column(Uuid(native_uuid=True), primary_key=True)
-
-    # cdb    : resolves to UUID
-    # sqlite : behaves like UUID, i.e. GET /.../<id> convert to UUID
-    # id = db.Column(UUID(as_uuid=True), primary_key=True)
-
-    # all resolve to CHAR
-    # POST /... convert UUID to str
-    # id = db.Column(db.String, primary_key=True)
-
-    # cdb    : resolves to CHAR
-    # sqlite : behaves like str, i.e. POST /... convert UUID to str
-    # id = db.Column(UUID(as_uuid=False), primary_key=True)
-
+    id = db.Column(Uuid(native_uuid=True), primary_key=True)
     url = db.Column(db.String, index=True, unique=True, nullable=False)
     created_at = db.Column(
         db.Date,
--- manage_db.py
+++ manage_db.py
@@ -12,24 +12,20 @@ from app.schema import db
 
 def view_database_details():
     """ View database details """
-    dialect_name = db.engine.dialect.name
-    print(f"Dialect : {dialect_name}")
-
-    tables_names = db.metadata.tables.keys()
-    print(f"Table Names: {tables_names}")
-    for table_name in tables_names:
-        table = db.metadata.tables[table_name]
-        print(table.name)
-        print(CreateTable(table))
-        print()
+    print(f"Dialect : {db.engine.dialect.name}")
+    for table_name in db.metadata.tables.keys():
+        table = db.metadata.tables.get(table_name)
+        print(str(CreateTable(table).compile(db.engine)))
 
 def parse_args():
     """ Parse the arguments """
     parser = argparse.ArgumentParser(description='Manage the database')
-    parser.add_argument('--reset', action='store_true',
-                        help='Reset the database')
     parser.add_argument('--view', action='store_true',
                         help='View the database tables')
+    parser.add_argument('--reset', action='store_true',
+                        help='Reset the database')
+    parser.add_argument('--remove', action='store_true',
+                        help='Reset the database')
     if len(argparse.Namespace().__dict__) > 1:
         sys.exit(parser.print_usage())
     return parser.parse_args()
@@ -41,13 +37,16 @@ if __name__ == '__main__':
         database_uri = app.config['SQLALCHEMY_DATABASE_URI']
         database_name = make_url(database_uri).database
         print(f"Database Name: {database_name}")
-        if args.reset:
-            print("Resetting database...")
+        if args.view:
+            pass
+        elif args.reset:
+            print("Resetting database tables...")
             db.drop_all()
             db.create_all()
-        elif args.view:
-            print("Viewing database details...")
-            view_database_details()
+        elif args.remove:
+            print("Removing database tables...")
+            db.drop_all()
         else:
-            print("Initializing database...")
+            print("Initializing database tables...")
             db.create_all()
+        view_database_details()
